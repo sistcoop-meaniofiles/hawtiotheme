b28ca2d87d6eb9a174535ad004eea5a693dbf122
Carlos <carlosthe19916@gmail.com>
2015-05-15 11:15:47 -0500

    add angular-ladda

--- app.js
+++ app.js
@@ -11,105 +11,127 @@ var Hawtiotheme = new Module('hawtiotheme');
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
  */
-Hawtiotheme.register(function(app, auth, database) {
+Hawtiotheme.register(function (app, auth, database) {
 
-  app.set('views', __dirname + '/server/views');
+    app.set('views', __dirname + '/server/views');
 
-  //We enable routing. By default the Package Object is passed to the routes
-  Hawtiotheme.routes(app, auth, database);
-
-
-  //meanio theme
-  Hawtiotheme.aggregateAsset('css', 'hawtiotheme.css');
-  Hawtiotheme.aggregateAsset('css', '../lib/openshift/dist/css/main.css', {global:true,  weight: -4, group: 'footer'});
-
-  //themecss
-  Hawtiotheme.aggregateAsset('css', '../lib/patternfly/dist/css/patternfly.css', {global:true,  weight: -5, group: 'footer'});
-  Hawtiotheme.aggregateAsset('css', '../lib/messenger/build/css/messenger-theme-flat.css', {global:true,  weight: -6, group: 'footer'});
-  Hawtiotheme.aggregateAsset('css', '../lib/kubernetes-label-selector/labelFilter.css', {global:true,  weight: -7, group: 'footer'});
-
-  //angular-messages
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-messages/angular-messages.js');
-  //angular-sanitize
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-sanitize/angular-sanitize.js');
-  //angular-animate
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-animate/angular-animate.js');
-  //angular-file-upload
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-file-upload/angular-file-upload.js');
-  //angular-ui-grid
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-grid/ui-grid.js');
-  Hawtiotheme.aggregateAsset('css', '../lib/angular-ui-grid/ui-grid.css');
-  //angular-ui-select
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-select/dist/select.js');
-  Hawtiotheme.aggregateAsset('css', '../lib/angular-ui-select/dist/select.css');
-  //angular-ui-select-utils
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-select-utils/dist/select.utils.js');
-  //angular-ui-utils
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-utils/ui-utils.js');
-  //angular-input-masks
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-input-masks/angular-input-masks.js');
-  //angular-toastr
-  Hawtiotheme.aggregateAsset('css', '../lib/angular-toastr/dist/angular-toastr.css');
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-toastr/dist/angular-toastr.tpls.js');
-  //angular-spinner
-  Hawtiotheme.aggregateAsset('js', '../lib/spin.js/spin.js', {global:true,  weight: -5, group: 'header'});
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-spinner/angular-spinner.js');
-  //angular-xeditable
-  Hawtiotheme.aggregateAsset('css', '../lib/angular-xeditable/dist/css/xeditable.css');
-  Hawtiotheme.aggregateAsset('js', '../lib/angular-xeditable/dist/js/xeditable.js');
-  //ng-autofocus
-  Hawtiotheme.aggregateAsset('js', '../lib/ng-autofocus/dist/ng-autofocus.js');
-  //restangular
-  Hawtiotheme.aggregateAsset('js', '../lib/underscore/underscore.js');
-  Hawtiotheme.aggregateAsset('js', '../lib/restangular/dist/restangular.js');
-  //sg-iso3166
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-iso3166/dist/sg-iso3166.js');
-  //sg-iso4217
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-iso4217/dist/sg-iso4217.js');
-  //sg-ubigeo
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-ubigeo/dist/sg-ubigeo.js');
-  //sg-persona
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-persona/dist/sg-persona.js');
-  //sg-producto
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-producto/dist/sg-producto.js');
-  //sg-rrhh
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-rrhh/dist/sg-rrhh.js');
-  //sg-cooperativa
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-cooperativa/dist/sg-cooperativa.js');
-  //sg-socio
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-socio/dist/sg-socio.js');
-  //sg-utils
-  Hawtiotheme.aggregateAsset('js', '../lib/sg-utils/dist/sg-utils.js');
+    //We enable routing. By default the Package Object is passed to the routes
+    Hawtiotheme.routes(app, auth, database);
 
 
-  Hawtiotheme.angularDependencies([
-    'mean.system',
-    'ngMessages',
-    'ngSanitize',
-    'ngAnimate',
-    'angularFileUpload',
-    'ui.grid',
-    'ui.grid.edit',
-    'ui.grid.selection',
-    'ui.select',
-    'ui.select.utils',
-    'ui.utils',
-    'ui.utils.masks',
-    'toastr',
-    'angularSpinner',
-    'xeditable',
-    'ng-autofocus',
-    'restangular',
-    'sg-iso3166',
-    'sg-iso4217',
-    'sg-ubigeo',
-    'sg-persona',
-    'sg-producto',
-    'sg-rrhh',
-    'sg-cooperativa',
-    'sg-socio',
-    'sg-utils'
-  ]);
+    //meanio theme
+    Hawtiotheme.aggregateAsset('css', 'hawtiotheme.css');
+    Hawtiotheme.aggregateAsset('css', '../lib/openshift/dist/css/main.css', {
+        global: true,
+        weight: -4,
+        group: 'footer'
+    });
 
-  return Hawtiotheme;
+    //themecss
+    Hawtiotheme.aggregateAsset('css', '../lib/patternfly/dist/css/patternfly.css', {
+        global: true,
+        weight: -5,
+        group: 'footer'
+    });
+    Hawtiotheme.aggregateAsset('css', '../lib/messenger/build/css/messenger-theme-flat.css', {
+        global: true,
+        weight: -6,
+        group: 'footer'
+    });
+    Hawtiotheme.aggregateAsset('css', '../lib/kubernetes-label-selector/labelFilter.css', {
+        global: true,
+        weight: -7,
+        group: 'footer'
+    });
+
+    //angular-messages
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-messages/angular-messages.js');
+    //angular-sanitize
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-sanitize/angular-sanitize.js');
+    //angular-animate
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-animate/angular-animate.js');
+    //angular-file-upload
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-file-upload/angular-file-upload.js');
+    //angular-ui-grid
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-grid/ui-grid.js');
+    Hawtiotheme.aggregateAsset('css', '../lib/angular-ui-grid/ui-grid.css');
+    //angular-ui-select
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-select/dist/select.js');
+    Hawtiotheme.aggregateAsset('css', '../lib/angular-ui-select/dist/select.css');
+    //angular-ui-select-utils
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-select-utils/dist/select.utils.js');
+    //angular-ui-utils
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-ui-utils/ui-utils.js');
+    //angular-input-masks
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-input-masks/angular-input-masks.js');
+    //angular-toastr
+    Hawtiotheme.aggregateAsset('css', '../lib/angular-toastr/dist/angular-toastr.css');
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-toastr/dist/angular-toastr.tpls.js');
+    //angular-spinner
+    Hawtiotheme.aggregateAsset('js', '../lib/spin.js/spin.js', {global: true, weight: -5, group: 'header'});
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-spinner/angular-spinner.js');
+    //angular-ladda
+    Hawtiotheme.aggregateAsset('js', '../lib/ladda/dist/spin.min.js', {global: true, weight: -7, group: 'header'});
+    Hawtiotheme.aggregateAsset('js', '../lib/ladda/dist/ladda.min.js', {global: true, weight: -6, group: 'header'});
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-ladda/dist/angular-ladda.min.js');
+    Hawtiotheme.aggregateAsset('css', '../lib/ladda/dist/ladda-themeless.min.css');
+    //angular-xeditable
+    Hawtiotheme.aggregateAsset('css', '../lib/angular-xeditable/dist/css/xeditable.css');
+    Hawtiotheme.aggregateAsset('js', '../lib/angular-xeditable/dist/js/xeditable.js');
+    //ng-autofocus
+    Hawtiotheme.aggregateAsset('js', '../lib/ng-autofocus/dist/ng-autofocus.js');
+    //restangular
+    Hawtiotheme.aggregateAsset('js', '../lib/underscore/underscore.js');
+    Hawtiotheme.aggregateAsset('js', '../lib/restangular/dist/restangular.js');
+    //sg-iso3166
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-iso3166/dist/sg-iso3166.js');
+    //sg-iso4217
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-iso4217/dist/sg-iso4217.js');
+    //sg-ubigeo
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-ubigeo/dist/sg-ubigeo.js');
+    //sg-persona
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-persona/dist/sg-persona.js');
+    //sg-producto
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-producto/dist/sg-producto.js');
+    //sg-rrhh
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-rrhh/dist/sg-rrhh.js');
+    //sg-cooperativa
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-cooperativa/dist/sg-cooperativa.js');
+    //sg-socio
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-socio/dist/sg-socio.js');
+    //sg-utils
+    Hawtiotheme.aggregateAsset('js', '../lib/sg-utils/dist/sg-utils.js');
+
+
+    Hawtiotheme.angularDependencies([
+        'mean.system',
+        'ngMessages',
+        'ngSanitize',
+        'ngAnimate',
+        'angularFileUpload',
+        'ui.grid',
+        'ui.grid.edit',
+        'ui.grid.selection',
+        'ui.select',
+        'ui.select.utils',
+        'ui.utils',
+        'ui.utils.masks',
+        'toastr',
+        'angularSpinner',
+        'angular-ladda',
+        'xeditable',
+        'ng-autofocus',
+        'restangular',
+        'sg-iso3166',
+        'sg-iso4217',
+        'sg-ubigeo',
+        'sg-persona',
+        'sg-producto',
+        'sg-rrhh',
+        'sg-cooperativa',
+        'sg-socio',
+        'sg-utils'
+    ]);
+
+    return Hawtiotheme;
 });
--- public/config/hawtiotheme.js
+++ public/config/hawtiotheme.js
@@ -2,46 +2,112 @@
 
 /* jshint -W098 */
 
-angular.module('mean.hawtiotheme').config(['$viewPathProvider', function($viewPathProvider) {
+angular.module('mean.hawtiotheme').config(['$viewPathProvider', function ($viewPathProvider) {
 
-  $viewPathProvider.override('system/views/index.html', 'hawtiotheme/views/index.html');
+    $viewPathProvider.override('system/views/index.html', 'hawtiotheme/views/index.html');
 
 }]);
 
-angular.module('mean.hawtiotheme').config(function($provide, sgKeycloakProvider) {
-  sgKeycloakProvider.restUrl = 'http://localhost:8080/auth/admin/realms/sistcoop';
+angular.module('mean.hawtiotheme').config(function ($provide, sgKeycloakProvider) {
+    sgKeycloakProvider.restUrl = 'http://localhost:8080/auth/admin/realms/sistcoop';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgIso3166Provider) {
-  sgIso3166Provider.restUrl = 'http://localhost:8080/iso3166/rest';
+angular.module('mean.hawtiotheme').config(function (sgIso3166Provider) {
+    sgIso3166Provider.restUrl = 'http://localhost:8080/iso3166/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgIso4217Provider) {
-  sgIso4217Provider.restUrl = 'http://localhost:8080/iso4217/rest';
+angular.module('mean.hawtiotheme').config(function (sgIso4217Provider) {
+    sgIso4217Provider.restUrl = 'http://localhost:8080/iso4217/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgUbigeoProvider) {
-  sgUbigeoProvider.restUrl = 'http://localhost:8080/ubigeo/rest';
+angular.module('mean.hawtiotheme').config(function (sgUbigeoProvider) {
+    sgUbigeoProvider.restUrl = 'http://localhost:8080/ubigeo/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgProductoProvider) {
-  sgProductoProvider.restUrl = 'http://localhost:8080/producto/rest';
+angular.module('mean.hawtiotheme').config(function (sgProductoProvider) {
+    sgProductoProvider.restUrl = 'http://localhost:8080/producto/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgPersonaProvider) {
-  sgPersonaProvider.restUrl = 'http://localhost:8080/persona/rest';
+angular.module('mean.hawtiotheme').config(function (sgPersonaProvider) {
+    sgPersonaProvider.restUrl = 'http://localhost:8080/persona/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgRrhhProvider) {
-  sgRrhhProvider.restUrl = 'http://localhost:8080/rrhh/rest';
+angular.module('mean.hawtiotheme').config(function (sgRrhhProvider) {
+    sgRrhhProvider.restUrl = 'http://localhost:8080/rrhh/rest';
 });
 
-angular.module('mean.hawtiotheme').config(function(sgCooperativaProvider) {
-  sgCooperativaProvider.restUrl = 'http://localhost:8080/cooperativa/rest';
+angular.module('mean.hawtiotheme').config(function (sgCooperativaProvider) {
+    sgCooperativaProvider.restUrl = 'http://localhost:8080/cooperativa/rest';
 });
 
-angular.module('mean.hawtiotheme').run(function(editableOptions, editableThemes) {
-  editableThemes.bs3.inputClass = 'input-sm form-control';
-  editableThemes.bs3.buttonsClass = 'btn-sm';
-  editableOptions.theme = 'bs3';
+angular.module('mean.hawtiotheme').run(function (editableOptions, editableThemes) {
+    editableThemes.bs3.inputClass = 'input-sm form-control';
+    editableThemes.bs3.buttonsClass = 'btn-sm';
+    editableOptions.theme = 'bs3';
+});
+
+angular.module('mean.hawtiotheme').run(function ($q, $rootScope, Restangular, toastr) {
+
+    //controllar el spiner del ladda button
+    $rootScope.control = {
+        block: false
+    };
+
+    //activa/desactiva el control en loader
+    Restangular.addFullRequestInterceptor(function (element, operation, route, url, headers, params, httpConfig) {
+        if (operation == 'post' || operation == 'put') {
+            $rootScope.control.block = true;
+        }
+    });
+    Restangular.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+        if (operation == 'post' || operation == 'put') {
+            $rootScope.control.block = false;
+        }
+        return data;
+    });
+
+    //no permite que objeto con estado false puedan hacer transacciones
+    Restangular.addFullRequestInterceptor(function (element, operation, route, url, headers, params, httpConfig) {
+        if (operation == 'post' || operation == 'put') {
+            if (angular.isDefined(element.estado) && element.estado == false) {
+                var defer = $q.defer();
+                defer.resolve('Objeto inactivo, no se puede actualizar.');
+                httpConfig.timeout = defer.promise;
+                return {
+                    element: element,
+                    headers: headers,
+                    params: params,
+                    httpConfig: httpConfig
+                };
+            }
+        }
+    });
+
+    Restangular.setErrorInterceptor(function (response, deferred, responseHandler) {
+        if (response.config.method == 'POST' || response.config.method == 'PUT') {
+            $rootScope.control.block = false;
+        }
+
+        if (response.status === 0) {
+            if (response.config.timeout) {
+                toastr.error(response.config.timeout.$$state.value);
+            } else {
+                toastr.error('No se pudo realizar la conexion al sistema, verifique que la base de datos este funcionando.');
+            }
+            return false; // error handled
+        }
+        if (response.status === 400) {
+            toastr.error('Bad request.');
+            return false; // error handled
+        }
+        if (response.status === 403) {
+            return false; // error handled
+        }
+        if (response.status === 405) {
+            alert("405");
+            return false; // error handled
+        }
+        return true; // error not handled
+    });
+
 });
